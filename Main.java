be3a7ed8a1778c09643ece9628816a974c9011fb
DevilRocky <aman.deadlyprince@gmail.com>
2018-10-26 00:46:00 +0530

    s

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("Hello World");
+        System.out.println("Hello " + name);
+        scan.close();
+
+    }
+}
